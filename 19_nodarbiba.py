d13ba2d019307d11b51937588ec9172f3d3af478
Ingmars Melkis <idmelkis@edu.riga.lv>
2024-03-04 09:04:54 +0200

    0403 kods

---
+++ 19_nodarbiba.py
@@ -0,0 +1,6 @@
+# import csv
+# with open(f'17_nodarbiba.csv', 'r') as f:
+#     csv_file = csv.reader(f, delimiter=' ')
+#     next(csv_file) # !!!
+#     for row in csv_file:
+#         print(row)
